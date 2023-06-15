6c0b9ecec53d04f5f35d3be28ca65493fd233525
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-06-15 23:14:23 +0530

    added jwt token generation, verfication and password verification logic

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,8 @@
 const UserRepository = require('../repository/userRepository');
-
 const userRepoObj = new UserRepository();
+const jwt = require('jsonwebtoken');
+const { JWT_KEY } = require('../config/serverConfig');
+const bcrypt = require('bcrypt');
 
 class UserService{
 
@@ -13,6 +15,55 @@ class UserService{
             throw error;
         }
     }
+
+    createToken(user){
+        try {
+            const response = jwt.sign(user,JWT_KEY, {expiresIn:'1h'});
+            return response;
+        } catch (error) {
+            console.log('Something went wrong in token creation');
+        }
+    }
+
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log('Something went wrong in token verification');
+        }
+    }
+
+    async isAuthenticated(token){
+        try {
+             const response = this.verifyToken(token);
+             if(!response){
+                 throw {error: "Invalid token"};
+             }
+ 
+             const user = await userRepositoryObj.getById(response.id);
+ 
+             if(!user){
+                 throw {error: "no user with given token exists"};
+             }
+ 
+             return user.id
+        } catch (error) {
+            console.log("something went wrong in auth process");
+            throw error;
+        }
+ 
+    }
+
+    comparePassword(plainPassword , encryptedPassword){
+        try {
+         return bcrypt.compareSync(plainPassword,encryptedPassword);
+        } catch (error) {
+            console.log("Something went wrong in passowrd comparison");
+            throw error;
+        }
+     }
+ 
 }
 
 module.exports = UserService
